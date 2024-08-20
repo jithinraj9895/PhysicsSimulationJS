8b476931aa54b21b4b902f46279d9f797da6296a
jithinraj9895 <jithinraj9895@gmail.com>
2024-08-20 18:00:51 +0530

    Added color select for balls

--- src/main.js
+++ src/main.js
@@ -23,7 +23,8 @@ function createBall(){
         engine.isLine = false;
     }
     console.log("the_value "+ getRandomInt(200,400));
-    const ball = new Ball(20, getRandomInt(200,500),getRandomInt(200,500)); 
+    const ball = new Ball(20, getRandomInt(200,500),getRandomInt(200,500));
+    ball.default_color = selectedColor;
     if(cor.value > 0)
         ball.restitution = cor.value;
     engine.bodies[counter] = ball;
@@ -51,16 +52,46 @@ function changeSelectedBall(){
     mousePointer.pointedBody.radius = r;
 }
 
+function createCustomBall(){
+    if(check.checked){
+        engine.isLine = true;
+    }else{
+        engine.isLine = false;
+    }
+    console.log("the_value "+ getRandomInt(200,400));
+    const ball = new Ball(20, getRandomInt(200,500),getRandomInt(200,500));
+    ball.default_color = selectedColor;
+    ball.velocity.x = vx.value;
+    ball.velocity.y = vy.value;
+    ball.acceleration.x = ax.value;
+    ball.acceleration.y = ay.value;
+    if(cor.value > 0)
+        ball.restitution = cor.value;
+    engine.bodies[counter] = ball;
+    counter++;
+}
+
 const button = document.getElementById('createball');
+const customBall = document.getElementById('createCustomBall');
 const boxButton = document.getElementById('createbox');
 const check = document.getElementById('addLinesChkBox');
 const cor = document.getElementById('cor');
+const vx = document.getElementById('vx');
+const vy = document.getElementById('vy');
+const ax = document.getElementById('ax');
+const ay = document.getElementById('ay');
 
 const changeCanvasBtn = document.getElementById('changeCanvas');
 const changeRadiusBtn = document.getElementById('radiusChange');
+let selectedColor = ""
 
+document.getElementById('colorPicker').addEventListener('input', function() {
+    selectedColor = this.value;
+    console.log(selectedColor);
+});
 
 // Attach the event listener
+customBall.addEventListener('click', createCustomBall);
 button.addEventListener('click', createBall);
 boxButton.addEventListener('click', createBoxs);
 changeCanvasBtn.addEventListener('click', changeCanvas);
